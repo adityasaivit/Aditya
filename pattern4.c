e95c5a66fafc56c2020d3376969124256ea12bbd
adityasaivit <113006142+adityasaivit@users.noreply.github.com>
2022-10-02 11:59:27 +0530

    Create pattern4.c

---
+++ pattern4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int k=1;
+    for(int i=1;i<=5;i++)
+    {
+        
+        for(int j=1;j<=i;j++)
+        {
+            printf("%d ",k);
+            k++;
+        }
+        
+        printf("\n");
+        
+    }
+    return 0;
+}
